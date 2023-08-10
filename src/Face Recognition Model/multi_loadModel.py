d66fc1795230e49da34427305d394f916d54534d
quandinh10 <dinhquan1003@gmail.com>
2023-08-10 20:58:26 +0700

    update publish function

--- src/Face Recognition Model/multi_loadModel.py
+++ src/Face Recognition Model/multi_loadModel.py
@@ -7,7 +7,6 @@ from model import FaceRecognition
 from keras_facenet import FaceNet
 from paho.mqtt import client as mqtt_client
 
-
 model_load_path = 'face_recognition_model.pkl'
 loaded_recognizer = FaceRecognition()
 
@@ -16,10 +15,40 @@ loaded_recognizer.load_model(model_load_path)
 # Global variables and locks for thread synchronization
 frame = None
 faces = None
-counter = 0
+counter=[0,0,0,0]
 exit_event = threading.Event()
 frame_lock = threading.Lock()
 faces_lock = threading.Lock()
+
+#-----------mqtt protocol----------------#
+
+#broker config
+broker = 'broker.hivemq.com'
+port = 1883
+topic = "to-esp8266"
+client_id = "quandinh10"
+counter = [0,0,0,0]
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+client = connect_mqtt()
+client.loop_start()
+
+def publish(client):
+    global counter
+    client.publish(topic,"open")
+    counter = [0,0,0,0]
+
 
 # Define the interval for face detection (perform detection every n frames)
 video = "http://192.168.1.5/cam-lo.jpg"
@@ -58,7 +87,23 @@ def display_frames():
 
                             if prob < 0.5:
                                 name = "UNKNOWN"
-
+                            else:
+                                if (name=="Nhan"):
+                                    counter[0]+=1
+                                    if (counter[0]==10):
+                                        publish(client)
+                                if (name=="Quan"):
+                                    counter[1]+=1
+                                    if (counter[1]==10):
+                                        publish(client)
+                                if (name=="Quy"):
+                                    counter[2]+=1
+                                    if (counter[2]==10):
+                                        publish(client)
+                                if (name=="Thinh"):
+                                    counter[3]+=1
+                                    if (counter[3]==10):
+                                        publish(client)
                             cv.rectangle(display_frame, (x, y), (x+w, y+h), (255, 0, 0), thickness=2)
                             cv.putText(display_frame, name, (x, y-20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), 2)
 
@@ -70,51 +115,23 @@ def display_frames():
         # Check for 'q' key press to exit the program
         if cv.waitKey(1) & 0xFF == ord('q'):
             exit_event.set()
+            client.loop_stop()
 
     cv.destroyAllWindows()
-
-#-----------mqtt protocol----------------#
-
-#broker config
-broker = 'broker.hivemq.com'
-port = 1883
-topic = "to-esp8266"
-client_id = "quandinh10"
-
-def connect_mqtt():
-    def on_connect(client, userdata, flags, rc):
-        if rc == 0:
-            print("Connected to MQTT Broker!")
-        else:
-            print("Failed to connect, return code %d\n", rc)
-
-    client = mqtt_client.Client(client_id)
-    client.on_connect = on_connect
-    client.connect(broker, port)
-    return client
-
-
-def publish(client):
-    return
-
-def run():
-    client = connect_mqtt()
-    client.loop_start()
-    publish(client)
-    client.loop_stop()
-
-
 
 # Create and start the threads
 fetch_thread = threading.Thread(target=fetch_frames_from_camera)
 process_thread = threading.Thread(target=process_frames)
 display_thread = threading.Thread(target=display_frames)
 
+
 fetch_thread.start()
 process_thread.start()
 display_thread.start()
+
 
 # Wait for the threads to finish
 fetch_thread.join()
 process_thread.join()
 display_thread.join()
+
