a7215b389133670f62770e042eab5f5e08d119d9
quandinh10 <dinhquan1003@gmail.com>
2023-08-11 20:33:51 +0700

    add mqtt config to multi_without_limit.py

--- src/Face Recognition Model/multi_without_limit.py
+++ src/Face Recognition Model/multi_without_limit.py
@@ -3,6 +3,8 @@ import numpy as np
 import requests
 import threading
 from model import FaceRecognition
+from paho.mqtt import client as mqtt_client
+
 
 train_path = "C:/Users/dinhq/Desktop/IoT-security-system-main/src/Face Recognition Model/train/"
 
@@ -18,15 +20,49 @@ exit_event = threading.Event()
 frame_lock = threading.Lock()
 faces_lock = threading.Lock()
 
+#-----------mqtt protocol----------------#
+
+#broker config
+broker = 'broker.hivemq.com'
+port = 1883
+topic = "to-esp8266"
+client_id = "quandinh10"
+counter = [0,0,0,0]
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+client = connect_mqtt()
+client.loop_start()
+
+def publish(client):
+    global counter
+    client.publish(topic,"open")
+    counter = [0,0,0,0]
+
+
 # Define the interval for face detection (perform detection every n frames)
-video = "http://192.168.1.9/cam-lo.jpg"
+# video = "http://192.168.1.9/cam-lo.jpg"
+video = cv.VideoCapture(0)
 def fetch_frames_from_camera():
     global frame
     while not exit_event.is_set():
-        response = requests.get(video)
-        if response.status_code == 200:
-            img_array = np.array(bytearray(response.content),dtype=np.uint8)
-            frame = cv.imdecode(img_array,-1)
+        isTrue,frame = video.read()
+        if not isTrue:
+            continue
+        # response = requests.get(video)
+        # if response.status_code == 200:
+        #     img_array = np.array(bytearray(response.content),dtype=np.uint8)
+        #     frame = cv.imdecode(img_array,-1)
 
 def process_frames():
     global frame, faces
@@ -43,7 +79,6 @@ def display_frames():
 
     while not exit_event.is_set():
         if frame is not None:
-            display_frame = frame.copy()
             # Draw face rectangles and labels on the display_frame
             with faces_lock:
                 if faces is not None:
@@ -55,18 +90,34 @@ def display_frames():
 
                             if prob < 0.5:
                                 name = "UNKNOWN"
-
-                            cv.rectangle(display_frame, (x, y), (x+w, y+h), (255, 0, 0), thickness=2)
-                            cv.putText(display_frame, name, (x, y-20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), 2)
+                            else:
+                                if (name=="Nhan"):
+                                    counter[0]+=1
+                                    if (counter[0]==5):
+                                        publish(client)
+                                elif (name=="Quan"):
+                                    counter[1]+=1
+                                    if (counter[1]==5):
+                                        publish(client)
+                                elif (name=="Quy"):
+                                    counter[2]+=1
+                                    if (counter[2]==5):
+                                        publish(client)
+                                else:
+                                    counter[3]+=1
+                                    if (counter[3]==5):
+                                        publish(client)
+                            cv.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), thickness=2)
+                            cv.putText(frame, name, (x, y-20), cv.FONT_HERSHEY_COMPLEX, 1.0, (0, 255, 0), 2)
 
                     except Exception as e:
                         print("Error:", e)
-
-            cv.imshow("Recognizer", display_frame)
+            cv.imshow("Recognizer", frame)
 
         # Check for 'q' key press to exit the program
         if cv.waitKey(1) & 0xFF == ord('q'):
             exit_event.set()
+            client.loop_stop()
 
     cv.destroyAllWindows()
 
@@ -75,9 +126,11 @@ fetch_thread = threading.Thread(target=fetch_frames_from_camera)
 process_thread = threading.Thread(target=process_frames)
 display_thread = threading.Thread(target=display_frames)
 
+
 fetch_thread.start()
 process_thread.start()
 display_thread.start()
+
 
 # Wait for the threads to finish
 fetch_thread.join()
